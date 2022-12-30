715ba357e9ca973ea0283bea542483997508d714
parkhwakyoung <firenoah.m@gmail.com>
2022-12-30 13:53:23 +0900

    마우스 좌표 확인

--- Card_Game/main.cpp
+++ Card_Game/main.cpp
@@ -14,18 +14,6 @@ int main(void) {
     // f : 소수
     // Vector2i : 정수
     Vector2i mouse_pos;// 마우스 좌표
-    int click_cnt = 0;    //마우스 누른 횟수
-
-    Font font;
-    font.loadFromFile("C:\\Windows\\Fonts\\Arial.ttf");
-
-    Text text;
-    text.setFont(font);
-    text.setCharacterSize(30);
-    text.setFillColor(Color::White);
-    text.setPosition(0, 0);
-    char info[40];
-
 
     while (window.isOpen())
     {
@@ -39,22 +27,10 @@ int main(void) {
             case Event::Closed:
                 window.close();
                 break;
-            case Event::MouseButtonPressed:
-                if (event.mouseButton.button == Mouse::Left) {
-                    click_cnt++;
-                }
             }
-            //한 번 누르면 한 번만 적용
-
         }
-        sprintf(info, "(%d, %d) click %d\n"
-            , mouse_pos.x, mouse_pos.y, click_cnt);// 마우스 x값 y값 출력
-        text.setString(info);
 
-        window.clear(Color::Black);
-        window.draw(text);
-
-        window.display();
+        printf("%d, %d", mouse_pos.x, mouse_pos.y);// 마우스 x값 y값 출력
     }
 
     return 0;
