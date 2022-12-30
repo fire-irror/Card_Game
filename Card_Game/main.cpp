fec559ff699d68577962bd7ba9805552b16d2a57
parkhwakyoung <firenoah.m@gmail.com>
2022-12-30 15:27:46 +0900

    카드 이미지 넣기

--- Card_Game/main.cpp
+++ Card_Game/main.cpp
@@ -16,8 +16,19 @@ struct Card {
 
 int main(void) {
 
-    int CARD_W = 200;
+    int CARD_W = 150;
     int CARD_H = 200;
+
+    Texture t[8 + 1];
+    t[0].loadFromFile("./resource/images/0.png");
+    t[1].loadFromFile("./resource/images/1.png");
+    t[2].loadFromFile("./resource/images/2.png");
+    t[3].loadFromFile("./resource/images/3.png");
+    t[4].loadFromFile("./resource/images/4.png");
+    t[5].loadFromFile("./resource/images/5.png");
+    t[6].loadFromFile("./resource/images/6.png");
+    t[7].loadFromFile("./resource/images/7.png");
+    t[8].loadFromFile("./resource/images/8.png");
 
     RenderWindow window(VideoMode(1200, 800), "Card_Game");// 화면 크기, 제목
     window.setFramerateLimit(60);// 60초마다 한 번씩 돌아간다
@@ -26,6 +37,7 @@ int main(void) {
     // Vector2i : 정수
     Vector2i mouse_pos;// 마우스 좌표
     int click_cnt = 0;    //마우스 누른 횟수
+    int n = 0;
 
     Font font;
     font.loadFromFile("C:\\Windows\\Fonts\\Arial.ttf");
@@ -42,7 +54,8 @@ int main(void) {
         for (int j = 0; j < S; j++) {
             cards[i][j].sprite.setSize(Vector2f(CARD_W, CARD_H));
             cards[i][j].sprite.setPosition(j * CARD_W, i * CARD_H);
-            cards[i][j].sprite.setFillColor(Color(i * 64, j * 64, 0));
+            cards[i][j].sprite.setTexture(&t[1+n/2]);
+            n++;
         }
     }
 
