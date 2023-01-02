759d4f2731a2c599afae802070ac65348b02f41e
parkhwakyoung <firenoah.m@gmail.com>
2023-01-02 11:48:41 +0900

    [문법] swap이 잘 되는 이유

--- Grammer/swap.c
+++ Grammer/swap.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-void swap(int *a, int *b);
+//_a,_b는 주소값, *_a, *_b는 가리키는 해당 변수의 실제값(int)
+void swap(int* _a, int* _b) {
+	int temp = *_a;
+	*_a = *_b;
+	*_b = temp;
+}
 int main() {
 	int a = 3;
 	int b = 5;
 
-	swap(&a, &b);
-
 	printf("a:%d  b:%d\n", a, b);
 
-	//a와 b를 swap하는 함수를 호출
+	swap(&a, &b);	//_a에 a의 주소값을, _b에게 b의 주소값을 넘겨준다
 	
 	printf("a:%d  b:%d\n", a, b);
-}
-void swap(int *a, int *b) {
-	int temp = (*b);
-	*b = (*a);
-	*a = temp;
 }
